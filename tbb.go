c7b7299de879a196affcf978628e2a85f84478f5
David Kleiven <davidkleiven446@gmail.com>
2022-12-09 17:54:42 +0100

    Add main file

---
+++ tbb.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"gotpb/gotpb"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Printf("Config file must be passed\n")
+		return
+	}
+
+	conf_file := os.Args[1]
+	log.Printf("Loading config from %s", conf_file)
+	gotpb.GetConf(conf_file)
+}
